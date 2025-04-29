dee241746cde319dc47977c6826c5a969dadf9cb
CamronJeffrey <camronjeffrey12@gmail.com>
2025-04-29 16:02:02 -0700

    Create adminMenu.hpp

---
+++ adminMenu.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <memory>
+#include <limits>
+#include <cctype>
+#include <fstream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+// Admin panel for viewing all account data
+void adminMenu();
+
