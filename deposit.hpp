9c16811b95a8926355c0e2799ca0b0ab19221a66
CamronJeffrey <camronjeffrey12@gmail.com>
2025-04-29 15:52:46 -0700

    Create deposit.hpp

---
+++ deposit.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <memory>
+#include <limits>
+#include <cctype>
+#include <fstream>
+#include <filesystem>
+
+
+    // Deposit money into the account if amount is within allowed limit
+    void deposit(double amount);
+
