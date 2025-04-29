b6bdbf770bbaced1d9884b3cd2e710ce370f0d2d
CamronJeffrey <camronjeffrey12@gmail.com>
2025-04-29 15:56:48 -0700

    Create clearInput.hpp

---
+++ clearInput.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <memory>
+#include <limits>
+#include <cctype>
+#include <fstream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+// Clears invalid input to avoid infinite loops
+void clearInput();
+
+
