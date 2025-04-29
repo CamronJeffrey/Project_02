bd9e474e381fd3df6050ae80eb8b841547c7969c
CamronJeffrey <camronjeffrey12@gmail.com>
2025-04-29 15:53:33 -0700

    withdraw.hpp

---
+++ withdraw.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <memory>
+#include <limits>
+#include <cctype>
+#include <fstream>
+#include <filesystem>
+
+
+    // Withdraw money from the account if conditions are met
+    void withdraw(double amount);
+
