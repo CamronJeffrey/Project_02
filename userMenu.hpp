7af7bf4188435bcc71b74dd3dfbfa9e3720496b7
CamronJeffrey <camronjeffrey12@gmail.com>
2025-04-29 16:01:30 -0700

    Create userMenu.hpp

---
+++ userMenu.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <memory>
+#include <limits>
+#include <cctype>
+#include <fstream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+// Displays options and processes input for normal users
+void userMenu();
+
