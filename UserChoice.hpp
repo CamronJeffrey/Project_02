06de8db6cfbd069615cf527bbd3d9442b559ce0d
CamronJeffrey <camronjeffrey12@gmail.com>
2025-04-29 16:00:15 -0700

    Create UserChoice.hpp

---
+++ UserChoice.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <memory>
+#include <limits>
+#include <cctype>
+#include <fstream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+// Enum for user menu choices to improve readability
+enum UserChoice {};
+
