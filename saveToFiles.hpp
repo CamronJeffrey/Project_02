05621e06ece92463d06e35c6ce30565089f71b11
CamronJeffrey <camronjeffrey12@gmail.com>
2025-04-29 15:58:56 -0700

    Create saveToFiles.hpp

---
+++ saveToFiles.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <memory>
+#include <limits>
+#include <cctype>
+#include <fstream>
+#include <filesystem>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+// Save all accounts to both text and binary files
+void saveToFiles();
+
